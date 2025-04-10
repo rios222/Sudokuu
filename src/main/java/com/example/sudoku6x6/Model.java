d201780aa67373fe9a34aff46761a1b01d0b9c5f
Jhojan Moreno <moreno.jhojan@correounivalle.edu.co>
2025-04-10 11:43:28 -0500

    ArrayList

--- src/main/java/com/example/sudoku6x6/Model.java
+++ src/main/java/com/example/sudoku6x6/Model.java
@@ -1,6 +1,29 @@
 package com.example.sudoku6x6;
 
+import java.util.ArrayList;
+
+
 public class Model {
+    ArrayList<String> lista = new ArrayList<>();
+
+    private void AnimacionTablero() {
+
+    }
+
+
+    private void Tablero() {
+
+        lista.add("Hola");
+        lista.add("Mundo");
+        String elemento = lista.get(0);  // "Hola"
+        lista.size();        // Número de elementos
+        lista.contains("Hola"); // true si el elemento está
+        lista.remove("Mundo");  // Elimina por valor
+        lista.remove(0);     // Elimina por índice
+        lista.clear();       // Vacía la lista
+
+
+    }
 
 
 }
