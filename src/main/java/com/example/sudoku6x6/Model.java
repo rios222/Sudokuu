40962cf27e72a245d871cc850c5d13be17740a0e
Jhojan Moreno <moreno.jhojan@correounivalle.edu.co>
2025-04-11 09:54:53 -0500

    Funcion de generacion del tablero arreglada

--- src/main/java/com/example/sudoku6x6/Model.java
+++ src/main/java/com/example/sudoku6x6/Model.java
@@ -1,25 +1,24 @@
 package com.example.sudoku6x6;
 
 import java.util.ArrayList;
-import java.util.Objects;
-
+import java.util.Arrays;
 
 public class Model {
-    ArrayList<ArrayList<String>> sudoku = new ArrayList<>(); //Este los va a guardar
-    ArrayList<ArrayList<String>> restricciones = new ArrayList<>(); // 0-5, 6-11, 12-17, 18-23, 24-29, 30-35 /
-    // X=1, Y=2, Z=3, A=4, B=5, C=6   restricciones.get(0).add("A"); restricciones.get(0).add("B");
-    int min = 1, max = 6;
-    int numeroAleatorio = (int) (Math.random() * (max - min + 1) + min); // Entre 1 y 6
+    ArrayList<ArrayList<String>> sudoku = new ArrayList<>();
 
+    ArrayList<ArrayList<String>> restriccionesFila = new ArrayList<>();
+    ArrayList<ArrayList<String>> restriccionesColumna = new ArrayList<>();
+    ArrayList<ArrayList<String>> restriccionesBloque = new ArrayList<>();
+
+    int min = 1, max = 6;
+    int numeroAleatorio;
     String letraBuscada = "";
     boolean encontrada = false;
 
-    //private void AnimacionTablero() {}
+    final ArrayList<String> letrasDisponibles = new ArrayList<>(Arrays.asList("X", "Y", "Z", "A", "B", "C"));
 
     public void numeroYletra() {
-        //Numero y letra
-        numeroAleatorio = (int) (Math.random() * (max - min + 1) + min); // Entre 1 y 6
-        //Definir la letra de restriccion
+        numeroAleatorio = (int) (Math.random() * (max - min + 1) + min);
         if (numeroAleatorio == 1) letraBuscada = "X";
         if (numeroAleatorio == 2) letraBuscada = "Y";
         if (numeroAleatorio == 3) letraBuscada = "Z";
@@ -28,65 +27,105 @@ public class Model {
         if (numeroAleatorio == 6) letraBuscada = "C";
     }
 
+    public int obtenerIndiceBloque(int fila, int columna) {
+        int filaBloque = fila / 2;
+        int colBloque = columna / 3;
+        return filaBloque * 2 + colBloque;
+    }
+
     public String[][] GenTablero() {
-        // Inicializar el tablero y las restricciones
-        sudoku.clear();
-        restricciones.clear();
-        for (int i = 0; i < 36; i++) {
-            sudoku.add(new ArrayList<>());
-            restricciones.add(new ArrayList<>());
-        }
-        String[][] clues = new String[6][6]; // ¡Este es el que se retorna!
+        String[][] clues = new String[6][6];
+        boolean tableroCompleto = false;
 
-        for (int i = 0; i < 6; i++) { // Inicializa el arreglo
-            for (int j = 0; j < 6; j++) {
-                clues[i][j] = "";
+        while (!tableroCompleto) {
+            // Limpiar todo
+            sudoku.clear();
+            restriccionesFila.clear();
+            restriccionesColumna.clear();
+            restriccionesBloque.clear();
+
+            for (int i = 0; i < 6; i++) {
+                restriccionesFila.add(new ArrayList<>());
+                restriccionesColumna.add(new ArrayList<>());
+                restriccionesBloque.add(new ArrayList<>());
             }
-        }
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
-                encontrada = false;
-                numeroYletra();
 
-                //como determino si se encuentra?
-                if (Objects.equals(letraBuscada, restricciones.get(i).get(j))) {
-                    encontrada = true;
+            for (int i = 0; i < 36; i++) {
+                sudoku.add(new ArrayList<>());
+            }
+
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < 6; j++) {
+                    clues[i][j] = "";
                 }
+            }
 
-                while (encontrada) {
-                    encontrada = false;
-                    numeroYletra();
-                    if (Objects.equals(letraBuscada, restricciones.get(i).get(j))) { // el error esta aqui
-                        encontrada = true;
+            boolean fallo = false;
+
+            for (int i = 0; i < 6 && !fallo; i++) {
+                for (int j = 0; j < 6 && !fallo; j++) {
+                    encontrada = true;
+                    int intentos = 50;
+                    int bloqueIndex = obtenerIndiceBloque(i, j);
+
+                    while (encontrada && intentos > 0) {
+                        numeroYletra();
+
+                        boolean enFila = restriccionesFila.get(i).contains(letraBuscada);
+                        boolean enColumna = restriccionesColumna.get(j).contains(letraBuscada);
+                        boolean enBloque = restriccionesBloque.get(bloqueIndex).contains(letraBuscada);
+
+                        if (!enFila && !enColumna && !enBloque) {
+                            encontrada = false;
+                        } else {
+                            intentos--;
                         }
                     }
 
-                if (!encontrada) { //Agregarla
-                    int posicionActual = i * 6 + j; //conversion a "vector" de una matriz
-                    sudoku.get(posicionActual).add(String.valueOf(numeroAleatorio)); //Agrega el numero al sudoku
+                    if (encontrada) {
+                        // Revisa todas las letras en orden
+                        boolean encontradaOrden = false;
+                        for (String letra : letrasDisponibles) {
+                            boolean enFila = restriccionesFila.get(i).contains(letra);
+                            boolean enColumna = restriccionesColumna.get(j).contains(letra);
+                            boolean enBloque = restriccionesBloque.get(bloqueIndex).contains(letra);
 
-                    restricciones.get(i).add(letraBuscada); // 0-1-2-3-4-5 vista en filas
-                    restricciones.get(i + 1).add(letraBuscada); // 6-7-8-9-10-11
-                    restricciones.get(i + 2).add(letraBuscada); // 12-13-14-15-16-17
-                    restricciones.get(i + 3).add(letraBuscada); // 18-19-20-21-22-23
-                    restricciones.get(i + 4).add(letraBuscada); // 24-25-26-27-28-29
-                    restricciones.get(i + 5).add(letraBuscada); // 30-31-32-33-34-35
-                    System.out.println(restricciones);
-                    //restricciones.get(j).add(letraBuscada); // 0-6-12-18-24-30 vista en columnas
-                    restricciones.get(j + 6).add(letraBuscada); // 1-7-13-19-25-31
-                    restricciones.get(j + 12).add(letraBuscada); // 2-8-14-20-26-32
-                    restricciones.get(j + 18).add(letraBuscada); // 3-9-15-21-27-33
-                    restricciones.get(j + 24).add(letraBuscada); // 4-10-16-22-28-34
-                    restricciones.get(j + 30).add(letraBuscada); // 5-11-17-23-29-35
-                    System.out.println(restricciones);
+                            if (!enFila && !enColumna && !enBloque) {
+                                letraBuscada = letra;
+                                numeroAleatorio = letrasDisponibles.indexOf(letra) + 1;
+                                encontradaOrden = true;
+                                encontrada = false;
+                                break;
+                            }
+                        }
 
-                    // restricciones.get(posicionActual).add(letraBuscada); //celdas 3x2
-                    clues[i][j] = String.valueOf(numeroAleatorio);
-                    System.out.println("La letra " + letraBuscada + " fue agregada.");
+                        if (!encontradaOrden) {
+                            // No hay letra válida, fallamos y reiniciamos el tablero
+                            fallo = true;
+                            break;
+                        }
+                    }
+
+                    if (!encontrada) {
+                        int posicionActual = i * 6 + j;
+                        sudoku.get(posicionActual).add(String.valueOf(numeroAleatorio));
+                        restriccionesFila.get(i).add(letraBuscada);
+                        restriccionesColumna.get(j).add(letraBuscada);
+                        restriccionesBloque.get(bloqueIndex).add(letraBuscada);
+                        clues[i][j] = String.valueOf(numeroAleatorio);
+
+                        System.out.println("Fila " + i + " Columna " + j + ": " + letraBuscada);
                     }
                 }
             }
-            System.out.println(restricciones);
-            return clues;
+
+            tableroCompleto = !fallo;
         }
+
+        System.out.println("Restricciones por fila: " + restriccionesFila);
+        System.out.println("Restricciones por columna: " + restriccionesColumna);
+        System.out.println("Restricciones por bloque: " + restriccionesBloque);
+
+        return clues;
     }
+}
